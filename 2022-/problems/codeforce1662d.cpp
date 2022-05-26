5c1d18d8b827223de3ae74d21d041b9ec8ca7728
hharryyf <harryheyifan99@gmail.com>
2022-05-26 10:44:12 +0800

    check secondary email deletion

---
+++ 2022-/problems/codeforce1662d.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+//#define D
+#ifdef D
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define debug(...)
+#endif
+typedef long long ll;
+#define MAX_SIZE 524312
+#define MID ((l+r)>>1)
+#define LEFT (index<<1)
+#define RIGHT (index<<1|1)
+using namespace std;
+
+int main() {
+
+    return 0;
+}
